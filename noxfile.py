c2e02c0ff9473a2d5f3dd99a9bb79ccf31322c79
Pradyun Gedam <pradyunsg@gmail.com>
2020-02-04 14:18:53 +0530

    Add PyPy + more visible output

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,19 @@
 import nox
 
 
-@nox.session(python=["2.7", "3.4", "3.5", "3.6", "3.7", "3.8"])
+@nox.session(python=["2.7", "3.4", "3.5", "3.6", "3.7", "3.8", "pypy", "pypy3"])
 def tests(session):
-    session.log("This is tests on...")
+    session.log("============ TESTS ============")
     session.run("python", "--version")
 
 
 @nox.session
 def lint(session):
-    session.log("This is lint on...")
+    session.log("============ LINT ============")
     session.run("python", "--version")
 
 
 @nox.session(python="3.8")
 def docs(session):
-    session.log("This is docs on...")
+    session.log("============ DOCS ============")
     session.run("python", "--version")
